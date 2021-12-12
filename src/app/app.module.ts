7e045891d2ff67ebfa9b476724710fcabe59ae6f
pardeep <pardeep.singh@orionesolution.in>
2021-12-12 13:11:07 +0530

    added article service and consumed get articles list api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,7 +42,8 @@ import { AddUpdateCommentComponent } from './add-update-comment/add-update-comme
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
